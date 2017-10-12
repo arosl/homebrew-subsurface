996d57aff2b5d4ccb33301f29596866b98b5d392
Andreas Rosland <andreas.rosland@copyleft.no>
2017-10-12 15:01:48 +0800

    lolz

--- subsurface.rb
+++ subsurface.rb
@@ -3,7 +3,7 @@ cask 'subsurface' do
   sha256 'e6d9c1e39e158ca9879625a758dca1c7b244a881113f4970ff3fa62f12d79df5'
 
   url "https://subsurface-divelog.org/downloads/Subsurface-#{version}.dmg"
-  name 'Subsurface'
+  name 'subsurface'
   homepage 'https://subsurface-divelog.org'
 
   app 'Subsurface/Subsurface.app'
