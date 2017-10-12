65234cf2efd1e8c7e5af29e74bd1ec19f6f120de
Andreas Rosland <andreas.rosland@copyleft.no>
2017-10-12 14:22:48 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+#homebrew tap for subsurface
+Tap to install the briliant diveloger subsurface on your mac with homebrew
---
+++ subsurface.rb
@@ -0,0 +1,10 @@
+cask 'Subsurface' do
+  version '4.6.4'
+  sha256 'e6d9c1e39e158ca9879625a758dca1c7b244a881113f4970ff3fa62f12d79df5'
+
+  url "https://subsurface-divelog.org/downloads/Subsurface-#{version.no_dots}.dmg"
+  name 'Subsurface'
+  homepage 'https://subsurface-divelog.org'
+
+  app 'Subsurface/Subsurface.app'
+end
